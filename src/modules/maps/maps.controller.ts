27bff6c9ad6ff1bf5bd64170afc63e18d4d0f2e6
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-03-22 18:59:44 -0400

    feat: add places types

--- src/modules/maps/maps.controller.ts
+++ src/modules/maps/maps.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { MapsService } from './maps.service';
 import { SearchNearbyQueryRequestDto } from './dto/search-nearby-query.request.dto';
+import { PlacesTypes } from './protocols/places.type';
 
 @Controller('maps')
 export class MapsController {
@@ -8,6 +9,12 @@ export class MapsController {
 
   @Get('search/nearby')
   async searchNearby(@Query() query: SearchNearbyQueryRequestDto) {
+    const isValidType = PlacesTypes.includes(query.type);
+
+    if (!isValidType) {
+      throw new BadRequestException('Invalid type');
+    }
+
     return this.mapsService.searchNearby(query);
   }
 }
